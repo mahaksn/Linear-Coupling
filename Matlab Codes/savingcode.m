05b35e06c34de8a0dc035fcbc6bfab4b9b8b8a35
MahashriN <91330438+mahaksn@users.noreply.github.com>
2024-06-19 12:25:34 +0530

    Update savingcode.m

--- Matlab Codes/savingcode.m
+++ Matlab Codes/savingcode.m
@@ -3,5 +3,5 @@ model = strcat(RD,'_',dim,'_',layer,'_',suffix);
 time = datestr(datetime('now'),'yyyymmdd_HHMMSS');
 filename = strcat(model,'_',time);
 
-folder = ['D:\Maha\MATLAB Codes\Linear Coupling\',model,'\',];
+folder = ['D:\Maha\MATLAB Codes\Linear Coupling\',model,'\',filename,'\',];
 end
